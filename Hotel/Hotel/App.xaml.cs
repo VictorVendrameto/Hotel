765ebec2b68fc9b652b5433f042583051ff3439f
Usuario <Usuario@CPD02-M05>
2022-08-24 15:43:41 -0300

    Adicionar arquivos de projeto.

---
+++ Hotel/Hotel/App.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Collections.Generic;
+
+using Hotel.View;
+using Hotel.Model;
+
+namespace Hotel
+{
+    public partial class App : Application
+    {
+        public List<UserData> list_user = new List<UserData>
+        {
+            new UserData()
+            {
+                Email = "victor@gmail",
+                Nome = "Victor",
+                Password = "4444"
+            },
+            new UserData()
+            {
+                Email = "vini@gmail",
+                Nome = "Vinicius",
+                Password = "5555"
+            }
+        };
+        public App()
+        {
+            InitializeComponent();
+
+            if (Properties.ContainsKey("usuario_logado"))
+            MainPage = new MainPage();
+            else MainPage = new Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Hotel/Hotel/Model/UserData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hotel.Model
+{
+    public class UserData
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Nome { get; set; }
+    }
+}

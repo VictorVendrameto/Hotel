47a66fe1284edfb164700f15e15cd3846781655a
VictorVendrameto <vendrametovictor@gmail.com>
2022-08-25 15:42:07 -0300

    Primeiro commit

--- Hotel/Hotel/View/Hospedagem.xaml.cs
+++ Hotel/Hotel/View/Hospedagem.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace Hotel.View
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Protegida : ContentPage
+    public partial class Hospedagem : ContentPage
     {
-        public Protegida()
+        public Hospedagem()
         {
             InitializeComponent();
         }
--- Hotel/Hotel/View/Login.xaml.cs
+++ Hotel/Hotel/View/Login.xaml.cs
@@ -12,14 +12,31 @@ namespace Hotel.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        App PropriedadesApp;
         public Login()
         {
             InitializeComponent();
+
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            PropriedadesApp = (App)Application.Current;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
+            string user = user_email.Text;
+            string password = user_pw.Text;
 
+            string user_correto = "victor@gmail";
+            string password_correto = "4444";
+
+
+            //vai pra pagina de hospedagem
+            if (user == user_correto && password == password_correto)
+            {
+                App.Current.Properties.Add("user_logado", user);
+                App.Current.MainPage = new MainPage();
+            }
         }
     }
 }

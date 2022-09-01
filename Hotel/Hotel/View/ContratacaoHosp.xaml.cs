7f605afc31e19b876213099486dd3df17cb2558f
VictorVendrameto <vendrametovictor@gmail.com>
2022-09-01 15:54:55 -0300

    Quarto commit - Login melhorado e junto com Contratação de Hospede

--- Hotel/Hotel/View/ContratacaoHosp.xaml.cs
+++ Hotel/Hotel/View/ContratacaoHosp.xaml.cs
@@ -21,6 +21,31 @@ namespace Hotel.View
             PropriedadesApp = (App)Application.Current;
 
             lbl_user.Text = App.Current.Properties["user_logado"].ToString();
+            
+            pck_suit.ItemsSource = PropriedadesApp.list_suit;
+            //checkin
+            dtp_checkin.MinimumDate = DateTime.Now;
+            dtp_checkin.MaximumDate = DateTime.Now.AddMonths(4);
+            //checkout
+            dtp_checkout.MinimumDate = DateTime.Now.AddDays(1);
+            dtp_checkout.MaximumDate = DateTime.Now.AddDays(1).AddMonths(4);
         }
-    }
+
+        private void dtp_checkin_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DatePicker elemento = (DatePicker)sender;
+
+            dtp_checkout.MinimumDate = elemento.Date.AddDays(1);
+            dtp_checkout.MaximumDate = elemento.Date.AddDays(1).AddMonths(4);
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSair_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }  
 }
